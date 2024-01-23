8bcff12a447be9e3d89c625627808664efc4b480
MerveKpln <hamuss001@gmail.com>
2024-01-23 15:19:46 +0400

    Hackerrank_Java

---
+++ src/End_Of_File.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class End_Of_File {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+    }
+}
---
+++ src/Static_Initializer_Block.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Static_Initializer_Block {
+
+     static boolean flag;
+     static int B,H;
+
+
+static {
+    Scanner input= new Scanner(System.in);
+    B= input.nextInt();
+    H= input.nextInt();
+    input.close();
+
+    if(B>0 && B<=100 && H>0 && H<100 ) flag=true;
+    else  System.out.println("java.lang.Exception: Breadth and height must be positive");;
+}
+
+    //2nd way
+//    flag=true;
+//    static{
+//        Scanner input= new Scanner(System.in);
+//        B= input.nextInt();
+//        H= input.nextInt();
+//        input.close();
+//        try {
+//            if(B<=0 || H<=0){
+//                flag=false;
+//                throw  new Exception("java.lang.Exception: Breadth and height must be positive");
+//            }
+//
+//        }catch (Exception e){
+//            System.exit(0);
+//        }
+//    }
+
+
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
+
